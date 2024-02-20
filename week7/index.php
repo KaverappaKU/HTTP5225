5599d6019fd88d432291e2d157f8eb6fc1ff4757
Kaverappa KU <caakash357@gmail.com>
2024-02-20 18:17:37 -0500

    adding week7 content

---
+++ week7/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <?php include('reusables/nav.php') ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3 class="display-5 mt-4 m-4">Login</h3>
+            </div>
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        
+    </div>
+</body>
+</html>
